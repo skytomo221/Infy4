5bb8589b058c37d56bb7a99e17e17b497370c705
skytomo <tomo1kawa.fullerene@gmail.com>
2019-10-24 16:19:09 +0900

    Add grab and grabbing mouse icon

--- DrawCanvas.java
+++ DrawCanvas.java
@@ -1,14 +1,17 @@
-import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelListener;
-import java.awt.event.MouseWheelEvent;
+
+import java.awt.Image;
+import java.awt.Toolkit;
 
 /**
  * ライフゲームをただひたすらに描画します。
@@ -65,6 +68,7 @@ public class DrawCanvas extends JPanel implements MouseListener, MouseMotionList
     }
 
     public void mouseEntered(MouseEvent e) {
+        // setCursor(Cursor.getPredefinedCursor(Cursor.CUSTOM_CURSOR));
     }
 
     public void mouseExited(MouseEvent e) {
@@ -72,15 +76,26 @@ public class DrawCanvas extends JPanel implements MouseListener, MouseMotionList
 
     public void mousePressed(MouseEvent e) {
         mouseStart = e.getPoint();
+        // マウスカーソル
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.getImage("icon/grabbing2.png");
+        Cursor c = toolkit.createCustomCursor(image, new Point(0, 0), "img");
+        setCursor(c);
     }
 
     public void mouseReleased(MouseEvent e) {
         padding.translate(-mouseStart.x + e.getPoint().x, -mouseStart.y + e.getPoint().y);
+        // マウスカーソル
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.getImage("icon/grab.png");
+        Cursor c = toolkit.createCustomCursor(image, new Point(0, 0), "img");
+        setCursor(c);
     }
 
     public void mouseDragged(MouseEvent e) {
         padding.translate(-mouseStart.x + e.getPoint().x, -mouseStart.y + e.getPoint().y);
         mouseStart.setLocation(e.getPoint());
+        repaint();
     }
 
     public void mouseMoved(MouseEvent e) {
@@ -92,5 +107,6 @@ public class DrawCanvas extends JPanel implements MouseListener, MouseMotionList
         } else if (e.getWheelRotation() < 0) {
             setCellSize(cellSize + 1);
         }
+        repaint();
     }
 }
