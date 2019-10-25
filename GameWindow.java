cf5933d6e5518d8af08eafab81c2103f240f3c36
skytomo <tomo1kawa.fullerene@gmail.com>
2019-10-25 17:25:57 +0900

    Add open file

--- GameWindow.java
+++ GameWindow.java
@@ -3,15 +3,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.LayoutManager;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -148,11 +152,30 @@ public class GameWindow extends JFrame implements MenuListener, ActionListener, 
             generationLabel.setText("Generation #1");
             lifeCountLabel.setText("Count: 0");
             drawCanvas.repaint();
+            drawCanvas.offsetPoint = new Point(0, 0);
+            drawCanvas.setCellSize(10);
         } else if (e.getSource() == menuitem2) {
-            ;
+            // ライフゲームのファイルを開く
+            JFileChooser filechooser = new JFileChooser("data");
+            int selected = filechooser.showOpenDialog(this);
+            if (selected == JFileChooser.APPROVE_OPTION) {
+                File file = filechooser.getSelectedFile();
+                try (FileInputStream f = new FileInputStream(file);
+                        BufferedInputStream b = new BufferedInputStream(f);
+                        ObjectInputStream in = new ObjectInputStream(b)) {
+                    drawCanvas.lifeGame = lifeGame = (LifeGame) in.readObject();
+                    generationLabel.setText("Generation #" + lifeGame.GetGeneration());
+                    lifeCountLabel.setText("Count: " + lifeGame.set.size());
+                    drawCanvas.repaint();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } catch (ClassNotFoundException cnfe) {
+                    cnfe.printStackTrace();
+                }
+            }
         } else if (e.getSource() == menuitem3) {
             // ライフゲームのファイルを保存
-            JFileChooser filechooser = new JFileChooser("");
+            JFileChooser filechooser = new JFileChooser("data");
             int selected = filechooser.showSaveDialog(this);
             if (selected == JFileChooser.APPROVE_OPTION) {
                 File file = filechooser.getSelectedFile();
